1d35668de129d5ed59d2e31c80753f08a90a844e
yogita-gkmit <yogita@gkmit.co>
2026-02-02 17:56:22 +0530

    fix: ssl issue on redis module

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,7 +1,8 @@
 import { Global, Module } from '@nestjs/common';
-import Redis from 'ioredis';
+import Redis, { Cluster } from 'ioredis';
 import { RedisHelper } from './redis.helper';
 import { RedisService } from './redis.service';
+
 @Global()
 @Module({
   providers: [
@@ -9,17 +10,22 @@ import { RedisService } from './redis.service';
       provide: 'REDIS_CLIENT',
       useFactory: () => {
         const isAws = process.env.IS_AWS === 'true';
-        return new Redis({
-          host: process.env.REDIS_HOST,
-          port: Number(process.env.REDIS_PORT ?? 6379),
-          ...(isAws ? { tls: {} } : {}),
-        });
+        if (isAws) {
+          return new Cluster([
+            { host: 'test-backend-redis-0001-001.test-backend-redis.wn13ev.aps1.cache.amazonaws.com', port: 6379 },
+            { host: 'test-backend-redis-0001-002.test-backend-redis.wn13ev.aps1.cache.amazonaws.com', port: 6379 },
+          ]);
+        } else {
+          return new Redis({
+            host: process.env.REDIS_HOST ?? '127.0.0.1',
+            port: Number(process.env.REDIS_PORT ?? 6379),
+          });
+        }
       },
     },
     RedisService,
     RedisHelper,
   ],
-
   exports: ['REDIS_CLIENT', RedisService, RedisHelper],
 })
 export class RedisConfigModule {}
