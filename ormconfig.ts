bc42a053e8fb085a02361797b99a733e02e23189
yogita-gkmit <yogita@gkmit.co>
2026-02-02 18:01:14 +0530

    fix: ormconfig for ssl

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,18 +4,38 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const isCompiled = __filename.endsWith('.js');
+const isRds =
+  process.env.DB_HOST?.includes('rds.amazonaws.com');
 
 export const config: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  port: Number(process.env.DB_PORT ?? 5432),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  ssl: process.env.IS_AWS === 'true' ? { rejectUnauthorized: false } : false,
+  ssl: isRds
+    ? { rejectUnauthorized: false }
+    : false,
+  extra: isRds
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
+
   schema: 'public',
-  entities: [isCompiled ? 'dist/**/*.entity.js' : 'src/**/*.entity.ts'],
-  migrations: [isCompiled ? 'dist/src/migrations/*.js' : 'src/migrations/*.ts'],
+
+  entities: [
+    isCompiled ? 'dist/**/*.entity.js' : 'src/**/*.entity.ts',
+  ],
+  migrations: [
+    isCompiled
+      ? 'dist/src/migrations/*.js'
+      : 'src/migrations/*.ts',
+  ],
+
   synchronize: process.env.DB_SYNCHRONIZE === 'true',
 };
 
